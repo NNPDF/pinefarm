c6065649a52e79c96d9a9474dbe1f2c81a6afa41
Alessandro Candido <candido.ale@gmail.com>
2022-12-14 18:11:51 +0100

    Import pinefarm from runcards repo

---
+++ src/pinefarm/cli/__init__.py
@@ -0,0 +1,2 @@
+from . import configs, info, install, list, merge, run, update
+from ._base import command
---
+++ src/pinefarm/cli/configs.py
@@ -0,0 +1,14 @@
+import rich
+
+from .. import configs
+from ._base import command
+
+
+@command.command("configs")
+def subcommand():
+    """Query loaded configurations."""
+    main()
+
+
+def main():
+    rich.print(configs.configs)
