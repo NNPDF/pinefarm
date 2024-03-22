b78073a6f082b332def7f1475aaa67bcc36ab54d
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2024-03-22 14:58:10 +0200

    Improve decide_external_tool

--- src/pinefarm/external/__init__.py
+++ src/pinefarm/external/__init__.py
@@ -1,55 +1,52 @@
 """External interfaces module.
 
 All imports of external interfaces are 'hidden'
-under the function ``decide_external_tool`` to avoid
-unnecessary installations of external codes.
+under the function ``decide_external_tool`` to avoid an
+eager import and thus unnecessary installations of external codes.
 """
 
 from ..configs import configs
+from .interface import External
 
 
-def decide_external_tool(dsname):
+def decide_external_tool(dsname: str) -> tuple[External, str]:
     """Decide the external tool to be used.
 
-    This function uses completely arbitrary reasons to select a tool.
+    The decisions are based on the existence of a `.yaml` file with a specific name.
 
     Parameters
     ----------
-        dsname: str
-            name of the pinecard
+    dsname:
+        name of the pinecard
 
     Returns
     -------
-        external_interface: External
-            external interface to be used
-        color:
-            color code of the interface
+    external_interface:
+        external interface to be used
+    color:
+        color code of the interface
     """
-    # The decisions are usually based on the existence of a `.yaml` file with a specific name
-    # or a prefix in the pinecard
-
-    # DIS with yadism
     if (configs["paths"]["runcards"] / dsname / "observable.yaml").exists():
-        from . import yad
+        from . import yad  # pylint: disable=import-outside-toplevel
 
         return yad.Yadism, "red"
 
     if (configs["paths"]["runcards"] / dsname / "vrap.yaml").exists():
-        from . import vrap
+        from . import vrap  # pylint: disable=import-outside-toplevel
 
         return vrap.Vrap, "green"
 
     if (configs["paths"]["runcards"] / dsname / "positivity.yaml").exists():
-        from . import positivity
+        from . import positivity  # pylint: disable=import-outside-toplevel
 
         return positivity.Positivity, "yellow"
 
     if (configs["paths"]["runcards"] / dsname / "integrability.yaml").exists():
-        from . import integrability
+        from . import integrability  # pylint: disable=import-outside-toplevel
 
         return integrability.Integrability, "brown"
 
     # Defaults to Madgraph
-    from . import mg5
+    from . import mg5  # pylint: disable=import-outside-toplevel
 
     return mg5.Mg5, "blue"
